2f63b03211db93a61e20b6304ae3752db2ad8d1d
donricardogosta <horvathdanii99@gmail.com>
2021-12-28 23:14:20 +0100

    Update config

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if(!config.get('jwtPrivateKey')){
 app.set('views', './app/views');
 app.set('view engine', 'pug');
 
-mongoose.connect('mongodb://localhost:27017/vir_szakdolgozat')
+mongoose.connect(config.get('dbConfig.host'))
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...',err))
 
@@ -30,31 +30,23 @@ app.use('/magori', magori)
 app.use('/', home);
 
 console.log('Application Name: '+config.get('name'));
-console.log('Mail Server: '+config.get('mail.host'));
-console.log('Mail Password: '+config.get('mail.password'));
 
 if(app.get('env')==='production'){
     const cors = require('cors');
     const https = require('https');
-    const http = require('http');
     const fs = require('fs');
     app.use(cors());
-    const httpServer = http.createServer(app);
     const httpsServer = https.createServer({
         key: fs.readFileSync('../../etc/letsencrypt/live/metalcoding.eu/privkey.pem'),
         cert: fs.readFileSync('../../etc/letsencrypt/live/metalcoding.eu/fullchain.pem'),
     }, app);
-    httpServer.listen(80, () => {
-        console.log('HTTP Server running on port 80');
-    });
 
-    httpsServer.listen(443, () => {
+    httpsServer.listen(config.get('port'), () => {
         console.log('HTTPS Server running on port 443');
     });
 }
 else if(app.get('env')==='development'){
     app.use(morgan('tiny'));//kiírja a (defaultként)console-ra hogy milyen requestek történtek és hogy milyen státuszkóddal végződtek
     console.log('Morgan enabled...');
-    port = 3000;
-    app.listen(port, ()=> console.log(`Listening in port ${port}...`));
+    app.listen(config.get('port'), ()=> console.log(`Listening in port ${config.get('port')}...`));
 }
