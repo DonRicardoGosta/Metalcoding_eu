8adb8f290a1abf2df292d45ceffc53c20f8b8fdc
Horváth Dániel <61006437+DonRicardoGosta@users.noreply.github.com>
2022-02-13 21:34:59 +0100

    Update igenyfelmeres_data_handler.js

--- app/control/database/igenyfelmeres_data_handler.js
+++ app/control/database/igenyfelmeres_data_handler.js
@@ -1,16 +1,15 @@
-const {User} = require("../../model/userModel");
 const {BrandModel} = require("../../model/igenyfelmeres_data_models/brands_enum_Model");
 const {DeviceModel} = require("../../model/igenyfelmeres_data_models/devices_enum_Model");
 const {FunctionInHouseModel} = require("../../model/igenyfelmeres_data_models/functions_enum_Model");
 const {LocationModel} = require("../../model/igenyfelmeres_data_models/locations_enum_Model");
 const {IgenyfelmeresRecordModel} = require("../../model/igenyfelmeres_data_models/igenyfelmeres_records_Model");
-const {IgenyfelmeresLineRecordModel} = require("../../model/igenyfelmeres_data_models/igenyfelmeres_line_records_Model");
-const {Project} = require("../../model/project-management-modells/projectModel");
+
 
 
 async function getIgenyfelmeresRecords(user_id){
-    if(user_id){
-        try {
+    try {
+        if(user_id){
+
             const igenyfelmeres_records = await IgenyfelmeresRecordModel
                 .find({sharedwith : user_id.toString()})
                 .populate({
@@ -21,9 +20,9 @@ async function getIgenyfelmeresRecords(user_id){
                 })
                 .select('name _id');
             return igenyfelmeres_records;
-        }catch (ex){
-            console.log(ex.message)
         }
+    }catch (ex){
+        console.log(ex.message)
     }
 }
 
