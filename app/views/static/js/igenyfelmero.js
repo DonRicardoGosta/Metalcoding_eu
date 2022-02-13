f8099315889035d8131bfbe382a40f5088b951f1
Horváth Dániel <61006437+DonRicardoGosta@users.noreply.github.com>
2022-02-13 11:55:54 +0100

    Update igenyfelmero.js

--- app/views/static/js/igenyfelmero.js
+++ app/views/static/js/igenyfelmero.js
@@ -30,10 +30,10 @@ function insertInputField(event){
 }
 function recreateNode(el, withChildren) {
     if (withChildren) {
-        el.parentNode.replaceChild(el.cloneNode(true), el);
+        el.parentElement.replaceChild(el.cloneNode(true), el);
     }
     else {
-        var newEl = el.cloneNode(false);
+        let newEl = el.cloneNode(false);
         while (el.hasChildNodes()) newEl.appendChild(el.firstChild);
         el.parentNode.replaceChild(newEl, el);
     }
