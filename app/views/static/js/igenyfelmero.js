11e406bb0fe57f5d88b558b209c835036f0dedc3
Horváth Dániel <61006437+DonRicardoGosta@users.noreply.github.com>
2022-02-13 10:18:02 +0100

    Feature: renameable text

--- app/views/static/js/igenyfelmero.js
+++ app/views/static/js/igenyfelmero.js
@@ -1,6 +1,26 @@
 initIgenyfelmero();
 
-function initIgenyfelmero(){
+async function initIgenyfelmero(){
+    await initPlussButton();
+    await setEventListenersOnFields();
+}
+function setEventListenersOnFields(){
+    let name_fields = document.querySelectorAll(".ifl-name");
+    for (let name_field of name_fields){
+        name_field.addEventListener('dblclick', insertInputField)
+    }
+}
+
+function insertInputField(event){
+    let origin_text= event.target.textContent;
+    let renamebale_text = `
+        <input id="ifl-renameable-field" type="text" value="${origin_text}">
+    `;
+    event.target.textContent="";
+    event.target.insertAdjacentHTML("beforeend", renamebale_text);
+}
+
+function initPlussButton(){
     let searchContainer = document.querySelector("#igenyfelmero-container");
     if(searchContainer){
         printPlussButtonToTheScreen(searchContainer);
