d88e033da42e5447e9838fe2695ebcabdffd6ff9
Horváth Dániel <61006437+DonRicardoGosta@users.noreply.github.com>
2022-02-13 17:07:45 +0100

    Update igenyfelmero.js

--- app/views/static/js/igenyfelmero.js
+++ app/views/static/js/igenyfelmero.js
@@ -20,9 +20,9 @@ function setEventListenersOnlocations(){
 async function optionChoosed(event){
     let option_id=event.target.value;
     showErrorMessage(event.target.value)
-    /*let line_record_id=event.target.parentElement.parentElement.querySelector(".ifl-id").textContent
+    let line_record_id=event.target.parentElement.parentElement.querySelector(".ifl-id").textContent
     let resp =await updateLineRecordOption(line_record_id, option_id);
-    showErrorMessage(resp);*/
+    showErrorMessage(resp.json());
 }
 function setEventListenersOnDescriptionFields(){
     let description_fields = document.querySelectorAll(".ifl-description");
