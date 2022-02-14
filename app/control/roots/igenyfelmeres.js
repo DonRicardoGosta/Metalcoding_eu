e9a2af436d27e5a7607a84dd32ffd2ef92d69072
Horváth Dániel <61006437+DonRicardoGosta@users.noreply.github.com>
2022-02-14 19:50:58 +0100

    Update igenyfelmeres.js

--- app/control/roots/igenyfelmeres.js
+++ app/control/roots/igenyfelmeres.js
@@ -15,23 +15,23 @@ router.get('/', auth, async (req,res) => {
 
     let user = null;
     if (await UserInSession(req)) user = await UserInSession(req);
-    const igenyfelmeres_records = await getIgenyfelmeresRecords(user._id);
+    /*const igenyfelmeres_records = await getIgenyfelmeresRecords(user._id);
     const active_igenyfelmero_record = igenyfelmeres_records[0];
     const lines_for_active_ifmero = active_igenyfelmero_record.line_records;
     const locations = await getLocations();
     const functions = await getFunctions();
     const devices = await getDevices();
-    const brands = await getBrands();
+    const brands = await getBrands();*/
     res.render('smarthome_igenyfelmeres', {
         title: 'MagoriCO - SmartHome - Igényfelmérés',
         user: user,
-        igenyfelmeres_records: igenyfelmeres_records,
+        /*igenyfelmeres_records: igenyfelmeres_records,
         active_igenyfelmero_record: active_igenyfelmero_record,
         lines_for_active_ifmero: lines_for_active_ifmero,
         locations: locations,
         functions: functions,
         devices: devices,
-        brands: brands
+        brands: brands*/
     });
     }catch (ex){
         console.log(ex.message);
